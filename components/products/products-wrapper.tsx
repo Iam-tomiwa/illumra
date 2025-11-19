90fe9e6ecb07e1c0fd59c843ead5fc7f7da4ae3f
iam-tomiwa <ajayiemmanuel630@gmail.com>
2025-11-19 16:01:45 +0100

    fix: unused vars

--- components/products/products-wrapper.tsx
+++ components/products/products-wrapper.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Icon } from "@iconify/react";
 import { Button } from "@/components/ui/button";
 import {
